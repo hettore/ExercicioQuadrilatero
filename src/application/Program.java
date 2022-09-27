70b45e051806d77dd74aa27cca438a3d35a9571d
Hettore Eduardo <duh.barretoo@gmail.com>
2022-09-27 16:31:52 -0300

    Usando return nos métodos.

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,13 +7,17 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Exercício quadrilátero");
-		Quadrilatero.area(3);
+		double areaQuadrado = Quadrilatero.area(3);
+		System.out.println("Área do quadrado: " + areaQuadrado);
 		System.out.println();
-		Quadrilatero.area(5d, 5d);
+		double areaRetangulo = Quadrilatero.area(5d, 5d);
+		System.out.println("Área do retângulo: " + areaRetangulo);
 		System.out.println();
-		Quadrilatero.area(7, 8, 9);
+		double areaTrapezio = Quadrilatero.area(7, 8, 9);
+		System.out.println("Área do trapézio: " + areaTrapezio);
 		System.out.println();
-		Quadrilatero.area(5f, 5f);
+		double areaLosango = Quadrilatero.area(5f, 5f);
+		System.out.println("Área do losango: " + areaLosango);
 	}
 
 	
